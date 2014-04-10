71885fb9bfc9c8030466d7daf043d46753d9d049
Tim Cinel <tcinel@atlassian.com>
2014-04-10 12:31:51 +1000

    BUILDENG-5298 Using tabwidth of 2 spaces for *.rb files

--- lib/puppet/provider/nexus_repository/ruby.rb
+++ lib/puppet/provider/nexus_repository/ruby.rb
@@ -1,15 +1,16 @@
 Puppet::Type.type(:nexus_repository).provide(:ruby) do
-    desc "Uses Ruby's rest library"
+  desc "Uses Ruby's rest library"
 
-    def create
-        # todo
-    end
+  def create
+    # todo
+  end
 
-    def destroy
-        # todo
-    end
+  def destroy
+    # todo
+  end
 
-    def exists?
-        false
-    end
+  def exists?
+    false
+  end
 end
+
