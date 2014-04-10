6bc0b755ddaa9fa982974303d1b3578f15067c58
Martin Meinhold <mmeinhold@atlassian.com>
2014-04-10 14:22:22 +1000

    BUILDENG-5324 Remove unused properties

--- lib/puppet/type/nexus_repository.rb
+++ lib/puppet/type/nexus_repository.rb
@@ -5,14 +5,4 @@ Puppet::Type.newtype(:nexus_repository) do
 
   newparam(:name, :namevar => true) do
   end
-  newproperty(:baseurl) do
-    desc "Base URL of service managing the resource"
-  end
-  newproperty(:resource) do
-    desc "Path to the REST resource, eg. '/api/users'"
-  end
-  newproperty(:timeout) do
-    desc "Time in seconds to wait for a response"
-    defaultto 60
-  end
 end
