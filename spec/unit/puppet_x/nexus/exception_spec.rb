584f055be3f0076d103d016260cd672074b7d939
Martin Meinhold <mmeinhold@atlassian.com>
2014-04-18 15:09:22 +1000

    BUILDENG-5279 Split out exception handling into separate class

---
+++ spec/unit/puppet_x/nexus/exception_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Nexus::ExceptionHandler do
+  describe 'retrieve_error_message' do
+    it { expect(Nexus::ExceptionHandler.retrieve_error_message('')).to eq('unknown') }
+    it { expect(Nexus::ExceptionHandler.retrieve_error_message(nil)).to eq('unknown') }
+    it { expect(Nexus::ExceptionHandler.retrieve_error_message('\n<html><p>Error</p></html>')).to eq('Error') }
+    it { expect(Nexus::ExceptionHandler.retrieve_error_message('{"errors": [{"msg": "Error" }]}')).to eq('Error') }
+    it { expect(Nexus::ExceptionHandler.retrieve_error_message({:errors => [{:msg => 'Error'}]})).to eq('Error') }
+    it { expect(Nexus::ExceptionHandler.retrieve_error_message({'errors' => [{'msg' => 'Error'}]})).to eq('Error') }
+  end
+end
