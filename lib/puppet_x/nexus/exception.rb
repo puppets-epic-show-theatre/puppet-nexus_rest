2aace4511148e67d6b55cd0e38bd8ce9b85d0704
Martin Meinhold <mmeinhold@atlassian.com>
2014-04-18 15:09:22 +1000

    BUILDENG-5279 Split out exception handling into separate class

---
+++ lib/puppet_x/nexus/exception.rb
@@ -0,0 +1,46 @@
+require 'json'
+
+module Nexus
+  class ExceptionHandler
+    def self.process(e)
+      msg = 'unknown'
+      msg = retrieve_error_message(e.http_body) if e.methods.include? :http_body
+      yield msg
+    end
+
+    def self.retrieve_error_message(data)
+      if data.nil? || data.empty?
+        return 'unknown'
+      elsif (data.is_a? String) && data.include?('<html>')
+        return data.match(/<p>(.*)<\/p>/)[1]
+      end
+
+      json = data
+      if data.is_a? String
+        begin
+          json = JSON.parse(data)
+        rescue
+          return 'unknown'
+        end
+      end
+        
+      # The data normally looks like
+      # {
+      #    "errors":
+      #    [
+      #        {
+      #            "id": "*",
+      #            "msg": "... <long error message> ..."
+      #        }
+      #    ]
+      # }
+      if json['errors']
+        json['errors'].collect {|entry| entry['msg'] }.join(' ')
+      elsif json[:errors]
+        json[:errors].collect {|entry| entry[:msg] }.join(' ')
+      else
+        'unknown'
+      end
+    end
+  end
+end
