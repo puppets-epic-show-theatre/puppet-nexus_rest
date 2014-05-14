b40529dc2aea57e4a2fa75b4e9af5ed2ce794999
Martin Meinhold <mmeinhold@atlassian.com>
2014-05-14 19:06:23 +1000

    BUILDENG-5278 Add a first version of the global settings type

---
+++ spec/unit/puppet/type/nexus_global_settings_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe Puppet::Type.type(:nexus_global_settings) do
+  let(:settings) { described_class.new(:name => 'default') }
+  specify 'by default' do
+    expect(settings[:notification_enabled]).to be_false
+    expect(settings[:notification_recipients]).to be_nil
+  end
+
+  describe 'notification_recipients' do
+    specify 'should support a single string' do
+      expect {
+        described_class.new(:name => 'default', :notification_recipients => 'jdoe@example.com')
+      }.to_not raise_error
+    end
+
+    specify 'should support an empty string' do
+      expect {
+        described_class.new(:name => 'default', :notification_recipients => "")
+      }.to_not raise_error
+    end
+
+    specify 'should support an empty array' do
+      expect {
+        described_class.new(:name => 'default', :notification_recipients => [])
+      }.to_not raise_error
+    end
+
+    specify 'should support multiple elements' do
+      expect {
+        described_class.new(:name => 'default', :notification_recipients => ['jdoe@example.com'])
+      }.to_not raise_error
+      expect {
+        described_class.new(:name => 'default', :notification_recipients => ['john@example.com', 'jane@example.com'])
+      }.to_not raise_error
+    end
+
+    specify 'should not accept invalid email addresses' do
+      expect {
+        described_class.new(:name => 'default', :notification_recipients => ['fail'])
+      }.to raise_error(Puppet::ResourceError, /Invalid email address 'fail'/)
+    end
+  end
+end
