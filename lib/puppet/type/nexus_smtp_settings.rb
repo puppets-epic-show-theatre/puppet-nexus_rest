a2bb9af943e1790a2cf20897f2964f1199fa6de2
Martin Meinhold <mmeinhold@atlassian.com>
2014-05-27 15:36:04 +1000

    BUILDENG-5616 Implement type

---
+++ lib/puppet/type/nexus_smtp_settings.rb
@@ -0,0 +1,55 @@
+require 'puppet/property/boolean'
+
+Puppet::Type.newtype(:nexus_smtp_settings) do
+  @doc = 'Manage the global Nexus SMTP settings.'
+
+  newparam(:name, :namevar => true) do
+    desc 'Name of the configuration (i.e. current).'
+  end
+
+  newproperty(:hostname) do
+    desc 'The host name of the SMTP server.'
+    validate do |value|
+      raise ArgumentError, "Hostname must not be empty" if value.nil? or value.to_s.empty?
+    end
+  end
+
+  newproperty(:port) do
+    desc 'The port number the SMTP server is listening on. Must be within 1 and 65535.'
+    defaultto 25
+    validate do |value|
+      raise ArgumentError, "Port must be a non-negative integer, got #{value}" unless value.to_s =~ /\d+/
+      raise ArgumentError, "Port must within [1, 65535], got #{value}" unless (1..65535).cover?(value.to_i)
+    end
+    munge { |value| Integer(value) }
+  end
+
+  newproperty(:username) do
+    desc 'The username used to access the SMTP server.'
+    defaultto ''
+  end
+
+  newproperty(:password) do
+    desc 'The password used to access the SMTP server. Use `absent` to express no password.'
+    defaultto :absent
+  end
+
+  newproperty(:communication_security) do
+    desc 'Enable the selected communication security when talking to the SMTP server. Can be one of: `none`, `ssl` or `tls`.'
+    defaultto :none
+    newvalues(:none, :ssl, :tls)
+  end
+
+  newproperty(:sender_email) do
+    desc 'Email address used in the `From:` field.'
+    validate do |value|
+      raise ArgumentError, "Sender email must not be empty" if value.nil? or value.to_s.empty?
+      raise ArgumentError, "Invalid email address '#{value}'." if value !~ /@/
+    end
+  end
+
+  newproperty(:use_nexus_trust_store, :parent => Puppet::Property::Boolean) do
+    desc 'When using SSL to communicate with the SMTP server, trust is based on Nexus\' internal certifacte store. Only valid for SSL connections.'
+    munge { |value| super(value).to_s.intern }
+  end
+end
