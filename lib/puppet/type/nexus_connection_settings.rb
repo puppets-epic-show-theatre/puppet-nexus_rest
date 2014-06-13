8b8ff17d6ec3ad142fdb594c8927dbb2a7977e77
Martin Meinhold <mmeinhold@atlassian.com>
2014-06-13 12:44:48 +1000

    BUILDENG-5713 Add type to manage global connection settings

---
+++ lib/puppet/type/nexus_connection_settings.rb
@@ -0,0 +1,42 @@
+Puppet::Type.newtype(:nexus_connection_settings) do
+  @doc = 'Manage the global connection settings.'
+
+  newparam(:name, :namevar => true) do
+    desc 'Name of the configuration (i.e. current).'
+  end
+
+  newproperty(:connection_timeout) do
+    desc 'Time in seconds Nexus will wait for a successful connection before retrying.'
+    defaultto 10
+    validate do |value|
+      raise ArgumentError, "Timeout must be a non-negative integer, got #{value}" unless value.to_s =~ /\d+/
+      raise ArgumentError, "Timeout must bigger than or equal to zero" unless value.to_i >= 0
+    end
+    munge { |value| Integer(value) }
+  end
+
+  newproperty(:connection_retries) do
+    desc 'Number of connection attempts before giving up.'
+    defaultto 3
+    validate do |value|
+      raise ArgumentError, "Retries must be a non-negative integer, got #{value}" unless value.to_s =~ /\d+/
+      raise ArgumentError, "Retries must bigger than or equal to zero" unless value.to_i >= 0
+    end
+    munge { |value| Integer(value) }
+  end
+
+  newproperty(:user_agent_fragment) do
+    desc 'A custom fragment to add to the `user-agent` string used in HTTP requests.'
+    defaultto ''
+  end
+
+  newproperty(:query_string) do
+    desc 'Additional parameters sent along with the HTTP request. They are appended to the url along with a `?`. E.g.
+      `foo=bar&foo2=bar2` becomes `http://myurl?foo=bar&foo2=bar2`.'
+    defaultto ''
+  end
+
+  autorequire(:file) do
+    Nexus::Config::CONFIG_FILENAME
+  end
+end
