aa0dfb17186caf3bab3933b17c332494fe027bd8
Tim Cinel <tcinel@atlassian.com>
2014-07-10 16:45:56 +1000

    BUILDENG-5685 Somewhat complete type for nexus_repository_route

--- lib/puppet/type/nexus_repository_route.rb
+++ lib/puppet/type/nexus_repository_route.rb
@@ -10,24 +10,39 @@ Puppet::Type.newtype(:nexus_repository_route) do
     desc 'Unique route identifier; once created cannot be changed unless the Repository Route is destroyed. This is not stored in Nexus.'
   end
 
-  newproperty(:group_id) do
-    desc 'The content provider of the Repository Group'
-    defaultto :maven2
-    newvalues(:maven1, :maven2, :nuget, :site, :obr)
+  newproperty(:id) do
+    desc 'Read only value used to manage the resource, do not specify this in the manifest.'
+    defaultto ''
   end
 
-  newproperty(:exposed, :parent => Puppet::Property::Boolean) do
-    desc 'Controls if the Repository Group is remotely accessible. Responds to the \'Publish URL\' setting in the UI.'
-    defaultto :true
-    munge { |value| super(value).to_s.intern }
+  newproperty(:position) do
+    desc 'The position of the route configuration in the list of route configurations. These should be unique integers beginning from 0 and incrementing by steps of 1.'
+    defaultto :0
+    munge { |value| Integer(value) }
+  end
+
+  newproperty(:url_pattern) do
+    desc 'Regular expression used to match the artifact path.'
+    defaultto ''
+  end
+
+  newproperty(:rule_type) do
+    desc 'Regular expression used to match the artifact path.'
+    defaultto :inclusive
+    newvalues(:inclusive, :exclusive, :blocking)
+  end
+
+  newproperty(:repository_group) do
+    desc 'The id of Repository Group that the route will be applied to'
+    defaultto ''
   end
 
   newproperty(:repositories, :array_matching => :all) do
-    desc 'A list of repositories contained in this Repository Group'
+    desc 'Ordered list list of repositories and repository_groups that should be considered when the pattern is matched for a query in '
     defaultto []
     validate do |value|
       unless value.empty?
-        raise ArgumentError, "repositories in group must be provided in an array" if value.include?(',')
+        raise ArgumentError, "repositories in route must be provided in an array" if value.include?(',')
       end
     end
   end
@@ -36,13 +51,19 @@ Puppet::Type.newtype(:nexus_repository_route) do
     Nexus::Config::file_path
   end
 
+  autorequire(:nexus_repository_group) do
+    self[:repositories] if self[:repositories] and self[:repositories].size() > 0
+  end
+
   autorequire(:nexus_repository) do
     self[:repositories] if self[:repositories] and self[:repositories].size() > 0
   end
 
   validate do
-    raise ArgumentError, "label must not be empty" if self[:ensure] == :present and (self[:label].empty?)
+    raise ArgumentError, "route position must be non-negative integer" if self[:position] < 0
+    raise ArgumentError, "route url_pattern must not be empty" if self[:position].empty?
+    raise ArgumentError, "route repository_group must not be empty" if self[:repository_group].empty?
+    raise ArgumentError, "route repositories list must not be empty" if self[:repositories].empty?
   end
 
 end
-
