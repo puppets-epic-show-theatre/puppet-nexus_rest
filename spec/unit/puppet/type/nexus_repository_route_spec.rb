d97a8174cb28b9b93b4c269462406720bdf10108
Tim Cinel <tcinel@atlassian.com>
2014-07-14 12:17:48 +1000

    BUILDENG-5685 Removed trailing commas for Ruby 1.8.7

--- spec/unit/puppet/type/nexus_repository_route_spec.rb
+++ spec/unit/puppet/type/nexus_repository_route_spec.rb
@@ -10,7 +10,7 @@ describe Puppet::Type.type(:nexus_repository_route) do
   it 'should reject non-integer name' do
     expect {
       Puppet::Type.type(:nexus_repository_route).new(
-        :name          => 'not a number',
+        :name          => 'not a number'
       )
     }.to raise_error(Puppet::ResourceError, /invalid value for Integer/)
   end
@@ -18,14 +18,14 @@ describe Puppet::Type.type(:nexus_repository_route) do
   it 'should reject non-integer position' do
     expect {
       Puppet::Type.type(:nexus_repository_route).new(
-        :position          => 'not a number',
+        :position          => 'not a number'
       )
     }.to raise_error(Puppet::ResourceError, /invalid value for Integer/)
   end
 
   it 'should accept an integer name' do
     Puppet::Type.type(:nexus_repository_route).new(
-      :position          => '0',
+      :position          => '0'
     )
   end
 
@@ -36,7 +36,7 @@ describe Puppet::Type.type(:nexus_repository_route) do
       :repository_group    => 'repo-group',
       :repositories        => 'repo',
 
-      :url_pattern       => 'some_pattern',
+      :url_pattern       => 'some_pattern'
     )
   end
 
@@ -48,7 +48,7 @@ describe Puppet::Type.type(:nexus_repository_route) do
         :repository_group  => 'repo-group',
         :repositories      => 'repo',
 
-        :url_pattern       => '',
+        :url_pattern       => ''
       )
     }.to raise_error(Puppet::Error, /route url_pattern must not be empty/)
   end
@@ -62,7 +62,7 @@ describe Puppet::Type.type(:nexus_repository_route) do
       :url_pattern         => 'some_pattern',
 
       #test
-      :rule_type           => :exclusive,
+      :rule_type           => :exclusive
     )
   end
 
@@ -75,7 +75,7 @@ describe Puppet::Type.type(:nexus_repository_route) do
         :repositories        => 'repo',
         :url_pattern         => 'some_pattern',
 
-        :repository_group    => '',
+        :repository_group    => ''
       )
     }.to raise_error(Puppet::Error, /route repository_group must not be empty/)
   end
@@ -89,7 +89,7 @@ describe Puppet::Type.type(:nexus_repository_route) do
         :repositories        => 'repo',
         :url_pattern         => 'some_pattern',
 
-        :repositories        => [],
+        :repositories        => []
       )
     }.to raise_error(Puppet::Error, /route repositories list must not be empty/)
   end
