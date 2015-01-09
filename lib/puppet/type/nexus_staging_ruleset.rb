6e96524af74a8ab7ea40ba3e09d059e8e7b6592b
Martin Meinhold <mmeinhold@atlassian.com>
2015-01-09 13:56:17 +1100

    BUILDENG-7292 Add interface of nexus_staging_ruleset

---
+++ lib/puppet/type/nexus_staging_ruleset.rb
@@ -0,0 +1,50 @@
+require 'puppet/property/keyvalue'
+require 'puppet/property/list'
+
+Puppet::Type.newtype(:nexus_staging_ruleset) do
+  @doc = 'Rules that can be associated with Nexus staging profiles.'
+
+  ensurable
+
+  newparam(:name, :namevar => true) do
+    desc 'Name of the scheduled task. Although Nexus allows to use the same name for multiple tasks, the module will
+      print a warning and ignore and subsequent task with the same name.'
+  end
+
+  newparam(:id) do
+    desc 'A read-only parameter set by the nexus_stating_ruleset resource.'
+  end
+
+  newproperty(:description) do
+    desc 'The description of the rule'
+    defaultto ''
+  end
+
+  newproperty(:rules, :parent => Puppet::Property::List) do
+    desc ''
+    validate do |value|
+      raise ArgumentError, 'Rules must not be empty' if value.to_s.empty?
+      raise ArgumentError, 'Multiple reccuring days must be provided as an array, not a comma-separated list.' if value.to_s.include?(',')
+    end
+    def membership
+      :inclusive_membership
+    end
+  end
+
+  validate do
+    if self[:ensure] == :present
+      raise ArgumentError, 'Rules is a mandatory property' if self[:rules].nil?
+      raise ArgumentError, 'Rules must not be empty' if self[:rules].to_s.empty?
+    end
+  end
+
+  newparam(:inclusive_membership) do
+    desc 'The list is considered a complete lists as opposed to minimum lists.'
+    newvalues(:inclusive)
+    defaultto :inclusive
+  end
+
+  autorequire(:file) do
+    Nexus::Config::file_path
+  end
+end
